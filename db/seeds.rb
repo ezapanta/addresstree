33831e3b08ed2ed853b38eb7a3f44146bb6fab80
ezapanta <zapanta.ed@gmail.com>
2025-10-13 12:42:46 -0500

    chore: profile see data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,91 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# db/seeds.rb
+
+Profile.destroy_all
+
+profiles = [
+  {
+    name: "Kai Cenat",
+    website: nil,
+    youtube: "https://www.youtube.com/@Kaicenat",
+    instagram: "https://www.instagram.com/Kaicenat/",
+    tiktok: "https://www.tiktok.com/@Kaicenat",
+    twitch: "https://www.twitch.tv/kaicenat",
+    username: "KaiCenat"
+  },
+  {
+    name: "Ludwig Ahgren",
+    website: "https://ludwig.gg",
+    youtube: "https://www.youtube.com/@Ludwig",
+    instagram: "https://www.instagram.com/LudwigAhgren/",
+    tiktok: "https://www.tiktok.com/@Ludwig",
+    twitch: "https://www.twitch.tv/ludwig",
+    username: "Ludwig"
+  },
+  {
+    name: "Hasan Piker",
+    website: nil,
+    youtube: "https://www.youtube.com/@HasanPiker",
+    instagram: "https://www.instagram.com/HasanAbi/",
+    tiktok: "https://www.tiktok.com/@HasanPiker",
+    twitch: "https://www.twitch.tv/hasanabi",
+    username: "Hasanabi"
+  },
+  {
+    name: "Case Baker",
+    website: nil,
+    youtube: "https://www.youtube.com/@caseoh_",
+    instagram: "https://www.instagram.com/caseoh_/",
+    tiktok: "https://www.tiktok.com/@caseoh_",
+    twitch: "https://www.twitch.tv/caseoh_",
+    username: "caseoh_"
+  },
+  {
+    name: "Jynxzi",
+    website: nil,
+    youtube: "https://www.youtube.com/@Jynxzi",
+    instagram: "https://www.instagram.com/Jynxzi/",
+    tiktok: "https://www.tiktok.com/@Jynxzi",
+    twitch: "https://www.twitch.tv/jynxzi",
+    username: "Nicholas Stewart"
+  },
+  {
+    name: "Jason Nguyen",
+    website: nil,
+    youtube: "https://www.youtube.com/@JasonTheWeen",
+    instagram: "https://www.instagram.com/JasonTheWeen/",
+    tiktok: "https://www.tiktok.com/@JasonTheWeen",
+    twitch: "https://www.twitch.tv/jasontheween",
+    username: "JasonTheWeen"
+  },
+  {
+    name: "Critical Role",
+    website: nil,
+    youtube: "https://www.youtube.com/@CriticalRole",
+    instagram: "https://www.instagram.com/CriticalRole/",
+    tiktok: "https://www.tiktok.com/@CriticalRole",
+    twitch: "https://www.twitch.tv/criticalrole",
+    username: "CriticalRole"
+  },
+  {
+    name: "Tyler Blevins",
+    website: nil,
+    youtube: "https://www.youtube.com/@Ninja",
+    instagram: "https://www.instagram.com/Ninja/",
+    tiktok: "https://www.tiktok.com/@Ninja",
+    twitch: "https://www.twitch.tv/ninja",
+    username: "Ninja"
+  },
+  {
+    name: "Miachel Grzesiek",
+    website: nil,
+    youtube: "https://www.youtube.com/@Shroud",
+    instagram: "https://www.instagram.com/Shroud/",
+    tiktok: "https://www.tiktok.com/@Shroud",
+    twitch: "https://www.twitch.tv/shroud",
+    username: "Shroud"
+  }
+]
+
+profiles.each do |profiles|
+  profiles.create!(celebrity)
+end
